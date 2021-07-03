b20141e4841166924e2c04f06165c070ed080125
OGA <totti0927.oga@gmail.com>
2021-07-03 20:53:58 +0900

    practice

--- components/atom/buttons/ButtonDefault.vue
+++ components/atom/buttons/ButtonDefault.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="button button-default">
+  <button 
+    :class="{ 'color-red': itemStatus === 'red'}"
+    class="button button-default"
+  >
     <p class="text">
       <slot>default</slot>
     </p>
@@ -8,7 +11,13 @@
 
 <script>
 export default {
-  
+  props: {
+    itemStatus: {
+      type: String,
+      required: false,
+      
+    }
+  },
 }
 </script>
 
@@ -17,6 +26,11 @@ export default {
     background-color: $color-font-sub;
     padding: 8px 16px;
     border-radius: 8px;
+    font-size: 12px;
+
+    &.color-red {
+      background-color: red;
+    }
 
     > .text {
       color: $color-font-sub;
---
+++ components/atom/texts/TextSub.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-sub">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message']
+}
+</script>
